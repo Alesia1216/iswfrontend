a4224ede64cb13d004a0fe6a9c9adb4f0cc316a7
Alesia <alesiasera04@gmail.com>
2025-01-24 18:36:41 +0100

    cansada de errores

--- src/app/service/session.service.ts
+++ src/app/service/session.service.ts
@@ -6,90 +6,95 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class SessionService {
+  subjectLogin: Subject<void> = new Subject<void>();
+    subjectLogout: Subject<void> = new Subject<void>();
 
-  subjectLogin: Subject<string> = new Subject<string>();
-  subjectLogout: Subject<string> = new Subject<string>();
+    private getToken(): string | null {
+        return localStorage.getItem('token');
+    }
 
-  constructor() { }
+    private deleteToken(): void {
+        localStorage.removeItem('token');
+    }
 
-  //login
-  private setToken(strToken: string): void {
-    localStorage.setItem('token', strToken);
-  }
+    isSessionActive(): boolean {
+        // comprobar si el token no ha expirado
+        const token = this.getToken();
+        if (token) {
+            let parsedToken: IJwt;
+            parsedToken = this.parseJwt(token);
+            const now = Date.now() / 1000;
+            if (parsedToken.exp > now) {
+                return true;
+            } else {
+                this.deleteToken();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
-  //logout
-  private deleteToken(): void {
-    localStorage.removeItem('token');
-  }
+    getSessionEmail(): string {
+        const token = this.getToken();
+        if (token) {
+            if (this.isSessionActive()) {
+                let parsedToken: IJwt;
+                parsedToken = this.parseJwt(token);
+                return parsedToken.email;
+            } else {
+                return '';
+            }
+        } else {
+            return '';
+        }
+    }
 
+    private parseJwt(token: string): IJwt {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload);
+    }
 
-  isSessionActive(): boolean {
-    const token = this.getToken();
-    if (token) {
-      let parsedToken = this.parseJwt(token);
-      const now = Date.now() / 1000;
-      if (parsedToken.exp > now) {
-        return true;
-      } else {
+    // private parseJwt(token: string): IJwt {
+    //   var base64Url = token.split('.')[1];
+    //   if (base64Url === undefined) {
+    //     console.log('Token invalido');
+    //     // create empty IJwt
+    //     return { jti: '', email: '', sub: '', iss: '', iat: 0, exp: 0 };
+    //   } else {
+    //     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    //     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    //       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    //     }).join(''));
+    //     return JSON.parse(jsonPayload);
+    //   }
+    // }
+
+    onLogin(): Subject<void> {
+        return this.subjectLogin;
+    }
+
+    onLogout(): Subject<void> {
+        return this.subjectLogout;
+    }
+
+    private setToken(strToken: string): void {
+        localStorage.setItem('token', strToken);
+    }
+
+    login(strToken: string): void {
+        this.setToken(strToken);
+        this.subjectLogin.next();
+    }
+
+    logout(): void {
         this.deleteToken();
-        return false;
-      }
-    } else {
-      return false;
+        this.subjectLogout.next();
     }
-  }
-
-  getSessionEmail(): string {
-    const token = this.getToken();
-    if (token) {
-      if (this.isSessionActive()) {
-        let parsedToken: IJwt = this.parseJwt(token);
-        return parsedToken.email;
-      } else {
-        return '';
-      }
-    } else {
-      return '';
-    }
-  }
-
-  private getToken(): string | null {
-    return localStorage.getItem('token');
-  }
-
-
-  private parseJwt(token: string): IJwt {
-    var base64Url = token.split('.')[1];
-    if (base64Url === undefined) {
-      console.log('Token invalido');
-      // create empty IJwt
-      return { jti: '', email: '', sub: '', iss: '', iat: 0, exp: 0 };
-    } else {
-      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      }).join(''));
-      return JSON.parse(jsonPayload);
-    }
-  }
-
-  onLogin(): Subject<string> {
-    return this.subjectLogin;
-  }
-
-  onLogout(): Subject<string> {
-    return this.subjectLogout;
-  }
-
-  login(strToken: string): void {
-    this.setToken('token');
-    this.subjectLogin.next('login');
-  }
-
-  logout(): void {
-    this.deleteToken();
-    this.subjectLogout.next('logout');
-  }
 
 
 }
