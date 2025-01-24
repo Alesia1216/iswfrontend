3a1fab6e96e71de8aec85d7bc283998de92896d0
Alesia <alesiasera04@gmail.com>
2025-01-24 18:50:29 +0100

    animacion un poco mas larga (hay que arreglarla porque parpadea un poco)

--- src/app/animations/animation.ts
+++ src/app/animations/animation.ts
@@ -3,11 +3,11 @@ import { trigger, transition, style, animate } from '@angular/animations';
 
 export const pageTransition = trigger('pageTransition', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'translateY(20px)' }),
-    animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+    style({ opacity: 0, transform: 'translateY(30px)' }),
+    animate('700ms cubic-bezier(0.25, 0.8, 0.25, 1)', style({ opacity: 1, transform: 'translateY(0)' }))
   ]),
   transition(':leave', [
-    animate('500ms ease-in', style({ opacity: 0, transform: 'translateY(-20px)' }))
+    animate('700ms cubic-bezier(0.25, 0.8, 0.25, 1)', style({ opacity: 0, transform: 'translateY(-30px)' }))
   ])
 ]);
 
