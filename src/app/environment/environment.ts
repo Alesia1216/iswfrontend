09d3fe648ac99f115d4a937e8ff1c1fb3d21c3c3
Alesia1216 <alesiasera04@gmail.com>
2025-06-13 20:58:31 +0200

    ya funciona el despligue (para volver a crear carpeta dist ng build)

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -4,7 +4,8 @@ export const environment = {
   production: false,
 };
 
-export const serverURL: string = 'http://localhost:8085';
+//export const serverURL: string = 'http://localhost:8085';
+export const serverURL: string = 'https://www.aleiswart.com/initial';
 
 export const httpOptions = {
   headers: new HttpHeaders({
